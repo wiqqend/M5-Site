34f42e1ac3081e556e24ac51f22bf8f02f95cf5a
Jacob <jacobdhartzell@gmail.com>
2026-02-05 13:57:30 -0500

    save

--- js03.js
+++ js03.js
@@ -15,3 +15,16 @@ let weekDays = [
   "Sunday", "Monday", "Tuesday", "Wednesday",
   "Thursday", "Friday", "Saturday"
 ];
+
+window.addEventListener("load", addWeekDays);
+
+function addWeekDays() {  
+  let i = 0;
+
+  let headingCells = document.getElementsByTagName("th");
+  while (i<7) {
+    headingCells[i].innerHTML = weekDays[i];
+    i++;
+  }
+}
+
